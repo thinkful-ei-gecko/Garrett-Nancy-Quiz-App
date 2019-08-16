c0447f1998b11ff3b1ad70ebfcbdb0b89e2b14cf
nphiukhao <nphiukhao@gmail.com>
2019-08-16 08:31:21 -0700

    files added

---
+++ index.js
@@ -0,0 +1,16 @@
+//const STORE with all the questions and answers
+
+//function for displaying/rendering the questions and answers 
+
+//function for checking the answers
+
+//function to store answer
+
+//function to "submit"
+
+//function to get result
+
+//function display the results
+
+//and then move to the next question
+
