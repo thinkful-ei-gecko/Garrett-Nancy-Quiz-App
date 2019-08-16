01f495048782306e74c36ed5f2f5081c6ca65627
Garrett James <garrettjames55@gmail.com>
2019-08-16 14:58:59 -0400

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+'use strict';
+
 //const STORE with all the questions and answers
 
+const STORE = {// currentQuestion, currentUserAnswer, question
+  currentUserAnswer: null,
+  currentQuestion: 0,
+  Questions: [{
+    question: 'Who created Slapsgiving?',
+    answer: ['Barney and Marshall', 'Robin and Lily', 'Ted and Barney', 'Ted and Marshall', 'Ted and Punchy'],
+    userAnswer: null,
+    correctAnswer: 0,
+    result: null,
+  }]
+};
+
 //function for displaying/rendering the questions and answers 
+function displayQA() {
+  // render the current question
+  let currentQuestion = STORE.currentQuestion;
+  // render question count
+  $('.question-count').text(`Question ${currentQuestion} of ${STORE.Questions.length}`);
+}
+
+console.log(displayQA());
 
 //function for checking the answers
 
@@ -14,3 +36,6 @@
 
 //and then move to the next question
 
+$(function(){
+  displayQA();
+});
