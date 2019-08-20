6580b156b80e40c08e4a43d63938621babddfdb6
Garrett James <garrettjames55@gmail.com>
2019-08-19 22:21:01 -0400

    quiz-App

--- index.js
+++ index.js
@@ -1,41 +1,135 @@
 'use strict';
 
-//const STORE with all the questions and answers
+let totalCorrect = 0;
+let currentQuestion = 0;
 
-const STORE = {// currentQuestion, currentUserAnswer, question
-  currentUserAnswer: null,
-  currentQuestion: 0,
-  Questions: [{
-    question: 'Who created Slapsgiving?',
-    answer: ['Barney and Marshall', 'Robin and Lily', 'Ted and Barney', 'Ted and Marshall', 'Ted and Punchy'],
-    userAnswer: null,
-    correctAnswer: 0,
-    result: null,
-  }]
-};
+const STORE = [
+  {
+    question: 'Who created Slapsgiving ?',
+    answer: '0',
+    choices: ['Barney and Marshall', 'Robin and Lily', 'Ted and Barney', 'Ted and Marshall', 'Ted and Punchy'],
+  },
+  {
+    question: 'Who was named Beercules ?',
+    answer: '2',
+    choices: ['Ted', 'Barney', 'Marshall', 'Lily', 'Punchy'],
+  },
+  {
+    question: 'Who was Lilys’ doppelganger ?',
+    answer: '3',
+    choices: ['Mustache lily', 'Street performer lily', 'Lily the wrestler', 'Stripper lily', 'Teacher lily'],
+  },
+  {
+    question: 'What’s Barneys job ?',
+    answer: '0',
+    choices: ['Please', 'Has a lot of keys', 'Stock investor', 'Lawyer', 'architect'],
+  },
+  {
+    question: 'Whats Robin famous for in Canada ?',
+    answer: '4',
+    choices: ['Actress', 'Model', 'News Reporter', 'Hockey player', 'Teenage popstar'],
+  },
+];
 
-//function for displaying/rendering the questions and answers 
-function displayQA() {
-  // render the current question
-  let currentQuestion = STORE.currentQuestion;
-  // render question count
-  $('.question-count').text(`Question ${currentQuestion} of ${STORE.Questions.length}`);
+$('.startButton').click(function() { // INIT QUIZ
+  $('.intro').hide();
+  $('.container').show();
+  $('.currentStats').show();
+  displayQuestion();
+  showCurrentStats();
+});
+
+$('.js-form').on('submit', function() {
+  event.preventDefault();
+  const userPickNum = $('input[name=user-answer]:checked').val();
+  const answerNum = STORE[currentQuestion].answer;
+  const answer = STORE[currentQuestion].choices[answerNum];
+  $('.results').show();
+  if (userPickNum === STORE[currentQuestion].answer) {
+    $('.results').html(`
+      <h2>Congratulations</h2>
+      <p>Omg! You are a How I Met Your Mother Legend!!</p>
+      <p>The answer is ${answer}.</p>
+      <button class="nextButton">Next</button>
+    `);
+    totalCorrect++;
+  } else {
+    $('.results').html(`
+      <h2>Incorrect</h2>
+      <p>The correct answer is ${answer}.</p>
+      <button class="nextButton" >Next</button>
+    `);
+  }
+  $('.question-container').hide();
+  currentQuestion++;
+  $('.checkAnswerButton').hide();
+});
+
+$('.results').on('click', '.nextButton', function() {
+  if (currentQuestion >= 5) {
+    $('.results').hide();
+    $('.container').hide();
+    $('.stats').show();
+    showStatsSection();
+    currentQuestion = 0;
+    totalCorrect = 0;
+  } else {
+    $('.container').show();
+    $('.results').hide();
+    displayQuestion();
+    showCurrentStats();
+  }
+});
+
+function displayQuestion() {
+  $('.question-container').show();
+  let answers = '';
+  for (let i = 0; i < 5; i++) {
+    answers += `
+      <div class="button">
+        <input type="radio" class="answerInput" name="user-answer" id="user-answer" value="${i}" aria-label="radioButton" required>${STORE[currentQuestion].choices[i]}
+      </div>
+    `;
+  }
+  
+  $('.question-container').html(`
+    <div class="questionDiv">
+      <h3>${STORE[currentQuestion].question}</h3>
+    </div>
+      <div class="answersDiv">
+          ${answers}      
+      </div>
+      <div class="checkAnswerButton">
+      <button type="submit" class="checkAnswer" >Submit</button></div>
+    </div>
+  `);
 }
 
-console.log(displayQA());
+$('.stats').on('click', '.restart-button', function(event) {
+  currentQuestion = 0;
+  totalCorrect = 0;
+  location.reload();
+});
 
-//function for checking the answers
 
-//function to store answer
+function showCurrentStats() {
+  let totalIncorrect = (currentQuestion) - totalCorrect;
+  $('.currentStats').html(`
+    <h2>Question ${currentQuestion + 1} of 5 <br>${totalCorrect} Correct   / ${totalIncorrect}  Incorrect</h2>
+  `);
+}
 
-//function to "submit"
+function showStatsSection() {
+  $('.curentStats').hide();
+  $('.stats').html(`
+    <h2> Here's how you did</h2>
+    <h2>${totalCorrect} correct out of 5 questions</h2>
+    <h3>Points Earned ${totalCorrect}</h3>
+    <button class="restart-button">Restart the Quiz</button>
+  `);
+}
 
-//function to get result
 
-//function display the results
-
-//and then move to the next question
-
-$(function(){
-  displayQA();
+$(function() { // Document.ready
+  $('.intro').show();
 });
